d97c3cab778a737ad315e7d47586503c960a75cc
thesharkreadabook <efride710@gmail.com>
2021-05-28 14:30:24 +0000

    wether datas insert to db

--- database.py
+++ database.py
@@ -24,15 +24,19 @@ def insert(key, datas):
                                                                                      datas[3], datas[4], datas[5], 
                                                                                      datas[6], datas[7], datas[8], 
                                                                                      datas[9], datas[10], datas[11])
+                cursor.execute(sql)
             elif key == 'weather':
-                sql = 'INSERT INTO `weather` (`baseDate`, `baseTime`, \
-                `category`, `fcstDate`, `fcstTime`, `fcstValue`, \
-                `nx`, `ny`) VALUES ("{}", "{}", "{}", "{}", "{}", "{}", {}, {})'.format(datas[0], datas[1], datas[2], 
-                                                                                        datas[3], datas[4], datas[5], 
-                                                                                        datas[6], datas[7])
+                print(datas)
+                for i in range(10):
+                    sql = 'INSERT INTO `weather` (`baseDate`, `baseTime`, \
+                    `category`, `fcstDate`, `fcstTime`, `fcstValue`, \
+                    `nx`, `ny`) VALUES ("{}", "{}", "{}", "{}", "{}", {}, {}, {})'.format(datas[i][0], datas[i][1], datas[i][2], 
+                                                                                            datas[i][3], datas[i][4], datas[i][5], 
+                                                                                            datas[i][6], datas[i][7])
+                    cursor.execute(sql)
             # elif key == 'news':
             #     sql = 
-            cursor.execute(sql)    
+            
             
         connection.commit()
         
